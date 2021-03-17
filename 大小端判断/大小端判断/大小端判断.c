36ca6c727034df3022361a2ba06b7493229f4b91
13579-liu <121309530@qq.com>
2021-03-17 10:27:59 +0800

    大小端判断.c

---
+++ 大小端判断/大小端判断/大小端判断.c
@@ -0,0 +1,26 @@
+﻿
+#include<stdio.h>
+//例：给出一个整数
+//  4  个字节
+//0x 00 00 00 01
+int check_sys()
+{
+	int a = 1;
+	char* p = (char*)&a;
+	if (*p == 1)
+		return 1;//小端
+	else
+		return 0;//大端
+}
+int main()
+{
+	if (check_sys() == 1)
+	{
+		printf("小端\n");
+	}
+	else
+	{
+		printf("大端\n");
+	}
+	return 0;
+}
